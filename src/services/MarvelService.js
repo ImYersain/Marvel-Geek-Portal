f1ee23397c5a326e497f0bbda77ea1cb9a04a9a0
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-02-14 00:05:33 +0100

    feat: random hero component, and date transformation

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,11 +12,22 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transfromCharacter(res));
     }
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+        return this._transfromCharacter(res.data.results[0]);
+    }
+    _transfromCharacter = (char) => {
+        return {
+            name : char.name,
+            description : char.description,
+            thumbnail : char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage : char.urls[0].url,
+            wiki : char.urls[1].url,
+        }
     }
 }
 
